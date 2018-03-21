246c7664b0fe42530076e1a3297871e5882a8c07
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 06:44:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,4 +1,9 @@
 def starts_with_a_vowel?(word)
+if word.scan(/\w[aeiouAEIOU]/) == nil
+    return false
+  else
+    return true
+  end
 
 end
 
@@ -7,7 +12,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+ text.match(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
