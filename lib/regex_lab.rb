c8b46e957f73e0ecd3824a773111de57a8fc3316
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 06:57:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text
+  if text.match(/^[A-Z].*\.$/) == nil
+    return false
+  else
+return true
+end
 end
 
 def valid_phone_number?(phone)
