25f965173ca3d515339ba2bee8af84ef28faf230
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 06:57:03 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
- text.match(/^\w{5}\s/)
+ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
