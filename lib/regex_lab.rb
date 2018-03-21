be56de771a1eafdadea9b79b7ee73dd78e389600
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 07:03:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-phone.match(/(?[0-9]/)
+ if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+  else
+    return true
+end
 end
